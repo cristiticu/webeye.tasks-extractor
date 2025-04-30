62794fbbf00949be4335d254d2f507e580a847d2
Cristian Ticu <cristian.ticu@eggnita.com>
2025-04-30 19:17:15 +0300

    Fix typing import on lambda

--- src/utils/sqs.py
+++ src/utils/sqs.py
@@ -1,9 +1,10 @@
 import json
-from typing import Mapping, Sequence
+from typing import TYPE_CHECKING, Mapping, Sequence
 from uuid import uuid4
 
 import boto3
-from types_boto3_sqs.type_defs import SendMessageBatchRequestEntryTypeDef
+if TYPE_CHECKING:
+    from types_boto3_sqs.type_defs import SendMessageBatchRequestEntryTypeDef
 
 
 def send_messages(queue_url: str, messages: Sequence[Mapping]):
@@ -12,7 +13,7 @@ def send_messages(queue_url: str, messages: Sequence[Mapping]):
     for i in range(0, len(messages), 10):
         chunk = messages[i: i + 10]
 
-        entries: list[SendMessageBatchRequestEntryTypeDef] = [
+        entries: list["SendMessageBatchRequestEntryTypeDef"] = [
             {
                 "Id": str(uuid4()),
                 "MessageBody": json.dumps(message),
